579f472d14a779f788582b3f4820aa05b2c4b2bb
gipye <gipyong7@gmail.com>
2023-08-01 07:33:12 +0000

    start project

---
+++ src/main/java/com/izi/er/ErApplication.java
@@ -0,0 +1,13 @@
+package com.izi.er;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/izi/er/ErApplicationTests.java
@@ -0,0 +1,13 @@
+package com.izi.er;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
